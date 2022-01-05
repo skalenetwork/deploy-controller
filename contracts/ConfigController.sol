6c7e2b7e00c3f23ef37c8c5a26cb1466134357eb
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-01-05 17:48:27 +0200

    SKALE-1263 Rename to ConfigController.sol

--- contracts/ConfigController.sol
+++ contracts/ConfigController.sol
@@ -2,11 +2,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 
-contract DeploymentController is AccessControlEnumerableUpgradeable {
+contract ConfigController is AccessControlEnumerableUpgradeable {
     bytes32 public constant DEPLOYER_ADMIN_ROLE = keccak256("DEPLOYER_ADMIN_ROLE");
     bytes32 public constant DEPLOYER_ROLE = keccak256("DEPLOYER_ROLE");
     bytes32 public constant MTM_ADMIN_ROLE = keccak256("MTM_ADMIN_ROLE");
-    bool public multiTransactionMode = false;
+    bool multiTransactionMode;
 
     function enableMTM() external {
         require(hasRole(MTM_ADMIN_ROLE, msg.sender), "Caller is not an admin");
@@ -28,5 +28,9 @@ contract DeploymentController is AccessControlEnumerableUpgradeable {
 
     function isAddressWhitelisted(address addr) external view returns (bool) {
         return hasRole(DEPLOYER_ROLE, addr) || AddressUpgradeable.isContract(addr);
+    }
+
+    function isMTMEnabled() external view returns (bool) {
+        return multiTransactionMode;
     }
 }
