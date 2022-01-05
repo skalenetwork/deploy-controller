38cb22159cdb2b598215c52f1a181c75698189ec
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-01-05 15:56:00 +0200

    SKALE-1263 Add turn on/off for MTM

--- contracts/DeploymentController.sol
+++ contracts/DeploymentController.sol
@@ -5,6 +5,18 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgrad
 contract DeploymentController is AccessControlEnumerableUpgradeable {
     bytes32 public constant DEPLOYER_ADMIN_ROLE = keccak256("DEPLOYER_ADMIN_ROLE");
     bytes32 public constant DEPLOYER_ROLE = keccak256("DEPLOYER_ROLE");
+    bytes32 public constant MTM_ADMIN_ROLE = keccak256("MTM_ADMIN_ROLE");
+    bool public multiTransactionMode = false;
+
+    function enableMTM() external {
+        require(hasRole(MTM_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        multiTransactionMode = true;
+    }
+
+    function disableMTM() external {
+        require(hasRole(MTM_ADMIN_ROLE, msg.sender), "Caller is not an admin");
+        multiTransactionMode = false;
+    }
 
     function addToWhitelist(address addr) external {
         grantRole(DEPLOYER_ROLE, addr);
