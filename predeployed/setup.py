cb56315643a6c71b55dbfef1b178fbf68c807766
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-06-29 17:50:29 +0300

    Update generator

--- predeployed/setup.py
+++ predeployed/setup.py
@@ -28,7 +28,7 @@ setup(
     author_email='support@skalelabs.com',
     url='https://github.com/skalenetwork/config-controller',
     install_requires=[
-        "predeployed-generator >= 0.0.1a4"
+        "predeployed-generator >= 1.1.0a0"
     ],
     python_requires='>=3.7,<4',
     extras_require=extras_require,
