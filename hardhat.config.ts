e3a09a42e816bf83c4f9bf85f35b1a51602ae978
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-05-10 12:21:52 +0300

    Add deployment script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,7 @@
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades"
 import { HardhatUserConfig } from "hardhat/config";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
