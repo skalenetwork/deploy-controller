2b8fb051335c1e43b0ffd05caf4b8539d7f54586
Dmytro Stebaiev <dimastebaev@gmail.com>
2024-05-13 18:10:23 +0300

    Add custom network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,39 @@ import '@typechain/hardhat'
 import { HardhatUserConfig } from "hardhat/config";
 
 
+function getCustomUrl(url: string | undefined) {
+  if (url) {
+    return url;
+  } else {
+    return "http://127.0.0.1:8545"
+  }
+}
+
+function getCustomPrivateKey(privateKey: string | undefined) {
+  if (privateKey) {
+    return [privateKey];
+  } else {
+    return [];
+  }
+}
+
+function getGasPrice(gasPrice: string | undefined) {
+  if (gasPrice) {
+    return parseInt(gasPrice, 10);
+  } else {
+    return "auto";
+  }
+}
+
+
 const config: HardhatUserConfig = {
+  networks: {
+    custom: {
+      url: getCustomUrl(process.env.ENDPOINT),
+      accounts: getCustomPrivateKey(process.env.PRIVATE_KEY),
+      gasPrice: getGasPrice(process.env.GASPRICE)
+    }
+  },
   solidity: "0.8.9",
   typechain: {
     target: 'ethers-v5'
