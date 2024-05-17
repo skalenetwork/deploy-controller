9991659e9c95b52d99df26977684f13129b16d0c
Dmytro Nazarenko <dmytro.nazarenko@skalelabs.com>
2024-05-17 16:03:09 +0100

    Update publish.sh

--- predeployed/scripts/publish.sh
+++ predeployed/scripts/publish.sh
@@ -7,7 +7,7 @@ if [ $TEST = 1 ]; then
 python3 -m twine upload --repository testpypi -u __token__ -p "$PYPI_TOKEN" dist/*
 else
 echo "Uploading to pypi"
-python3 -m twine upload -u __token__ -p "$PYPI_TOKEN" dist/*
+python3 -m twine upload -u "$PIP_USERNAME" -p "$PIP_PASSWORD" dist/*
 fi
 
 echo "==================================================================="
